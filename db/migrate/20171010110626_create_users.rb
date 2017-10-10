ccfe70265707381eb34ab2bac57a1b64ff70cb51
Miguel Angel Ibarra, Jr <miggs.ibarra@gmail.com>
2017-10-10 11:10:11 -0700

    copies migrations from the initial attemtp at the simple CRUD app.

---
+++ db/migrate/20171010110626_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    def change
+      create_table :users do
+        t.string :username, null: false
+        t.string :password_hash, null: false
+
+        t.timestamps
+      end
+    end
+  end
+end
