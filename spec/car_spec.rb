dfe695f87af9ebadb1b4ecd23790e4da3e68061f
Miguel Angel Ibarra, Jr <miggs.ibarra@gmail.com>
2017-10-10 15:30:50 -0700

    is not valid without a make

--- spec/car_spec.rb
+++ spec/car_spec.rb
@@ -1,5 +1,22 @@
 require 'spec_helper'
 
 describe Car do
-  pending "add some examples to (or delete) /Users/apprentice/Documents/Simple-CRUD-App2/Rakefile"
+  let(:car_example) { Car.new(
+      make: "Subaru",
+      model: "Impresa STI",
+      category: "compact-sport",
+      top_speed: 150,
+      horsepower: 310
+    ) }
+
+  describe "validations" do
+    it "is not valid without a make" do
+      car_example.make = ""
+      expect(car_example).to_not be_valid
+    end
+  end
+
+  describe "associations" do
+
+  end
 end
