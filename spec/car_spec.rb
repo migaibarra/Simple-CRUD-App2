92d8041ef341d636fe82056b8b801edec52f716b
Miguel Angel Ibarra, Jr <miggs.ibarra@gmail.com>
2017-10-10 15:37:19 -0700

    car belongs to a user

--- spec/car_spec.rb
+++ spec/car_spec.rb
@@ -14,9 +14,36 @@ describe Car do
       car_example.make = ""
       expect(car_example).to_not be_valid
     end
+
+    it "is not valid without a model" do
+      car_example.model = ""
+      expect(car_example).to_not be_valid
+    end
+
+    it "is not valid without a category" do
+      car_example.category = ""
+      expect(car_example).to_not be_valid
+    end
+
+    it "is not valid without a top_speed" do
+      car_example.top_speed = nil
+      expect(car_example).to_not be_valid
+    end
+
+    it "is not valid without horsepower" do
+      car_example.horsepower = nil
+      expect(car_example).to_not be_valid
+    end
   end
 
   describe "associations" do
-
+    let(:user_example) { User.create(
+      username: "loveChild88",
+      password: "nothing")
+    }
+    it "belongs to a user" do
+      user_example.cars << car_example
+      expect(car_example.user_id).to eq(user_example.id)
+    end
   end
 end
